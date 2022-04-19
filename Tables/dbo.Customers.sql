98358c5deda209c028c4feee855b3e3ecd26fbac
NicHopper <Nic.Hopper@Atrium-uw.com>
2022-04-19 15:44:03 +0100

    me
    commit 3.

---
+++ Tables/dbo.Customers.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Customers] (
+		[CustomerID]       [int] IDENTITY(1, 1) NOT NULL,
+		[CustomerName]     [varchar](40) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Customers] SET (LOCK_ESCALATION = TABLE)
+GO
