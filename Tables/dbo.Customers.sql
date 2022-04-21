dca0b29ee99e4ed58b8d69979c440c528e0bd961
nic.hopper@Atrium-uw.com <nic.hopper@Atrium-uw.com>
2022-04-21 10:40:22 +0100

    check in 45654546

--- Tables/dbo.Customers.sql
+++ Tables/dbo.Customers.sql
@@ -5,7 +5,8 @@ GO
 CREATE TABLE [dbo].[Customers] (
 		[CustomerID]       [int] IDENTITY(1, 1) NOT NULL,
 		[CustomerName]     [varchar](40) COLLATE Latin1_General_CI_AS NULL,
-		[CustomerDOB]      [int] NOT NULL
+		[CustomerDOB]      [int] NOT NULL,
+		[Text]             [nchar](10) COLLATE Latin1_General_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Customers] SET (LOCK_ESCALATION = TABLE)
