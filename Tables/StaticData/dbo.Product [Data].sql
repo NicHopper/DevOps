1c22a26c226070c7180bfbbe9f85030f7cb034f0
nic.hopper@Atrium-uw.com <nic.hopper@Atrium-uw.com>
2022-04-22 10:37:46 +0100

    added sample data for products

---
+++ Tables/StaticData/dbo.Product [Data].sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (1, 'Xbox')
+INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (2, 'PS5')
+INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (3, 'PC')
