1c3aee275d51d45ab73472b06f9d6fd32dd62f67
nic.hopper@Atrium-uw.com <nic.hopper@Atrium-uw.com>
2022-04-22 10:41:03 +0100

    Test changes

--- Tables/StaticData/dbo.Product [Data].sql
+++ Tables/StaticData/dbo.Product [Data].sql
@@ -1,3 +1,4 @@
-INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (1, 'Xbox')
+INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (1, 'Xbox Series X')
 INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (2, 'PS5')
 INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (3, 'PC')
+INSERT INTO [dbo].[Product] ([ProductID], [ProductName]) VALUES (4, 'Gameboy')
--- Tables/dbo.Order.sql
+++ Tables/dbo.Order.sql
@@ -4,7 +4,8 @@ SET ANSI_PADDING OFF
 GO
 CREATE TABLE [dbo].[Order] (
 		[OrderID]       [int] IDENTITY(1, 1) NOT NULL,
-		[ProductID]     [int] NOT NULL
+		[ProductID]     [int] NOT NULL,
+		[OrderQty]      [int] NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Order] SET (LOCK_ESCALATION = TABLE)
